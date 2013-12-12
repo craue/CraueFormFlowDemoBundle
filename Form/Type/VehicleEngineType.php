737b47c2744c12c48a33fd545c6a01c42918ef38
Christian Raue <christian.raue@gmail.com>
2013-12-12 22:33:27 +0100

    removed unneeded import

--- Form/Type/VehicleEngineType.php
+++ Form/Type/VehicleEngineType.php
@@ -4,7 +4,6 @@ namespace Craue\FormFlowDemoBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceListInterface;
-use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
