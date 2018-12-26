c8e81e021b626b831ecc113fb31a6638a538823b
Christian Raue <christian.raue@gmail.com>
2018-12-26 01:55:22 +0100

    removed dependency on SensioFrameworkExtraBundle

--- Controller/FormFlowDemoController.php
+++ Controller/FormFlowDemoController.php
@@ -7,9 +7,8 @@ use Craue\FormFlowDemoBundle\Entity\Location;
 use Craue\FormFlowDemoBundle\Entity\PhotoUpload;
 use Craue\FormFlowDemoBundle\Entity\Topic;
 use Craue\FormFlowDemoBundle\Form\CreateVehicle;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/CraueFormFlow")
@@ -22,67 +21,66 @@ class FormFlowDemoController extends Controller {
 
 	/**
 	 * @Route("/", name="_FormFlow_start")
-	 * @Template
 	 */
 	public function indexAction() {
-		return array();
+		return $this->render('@CraueFormFlowDemo/FormFlowDemo/index.html.twig');
 	}
 
 	/**
 	 * @Route("/create-location/", name="_FormFlow_createLocation")
-	 * @Template
 	 */
 	public function createLocationAction() {
-		return $this->processFlow(new Location(), $this->get('craueFormFlowDemoBundle.form.flow.createLocation'));
+		return $this->processFlow(new Location(), $this->get('craueFormFlowDemoBundle.form.flow.createLocation'),
+				'@CraueFormFlowDemo/FormFlowDemo/createLocation.html.twig');
 	}
 
 	/**
 	 * @Route("/create-vehicle/", name="_FormFlow_createVehicle")
-	 * @Template
 	 */
 	public function createVehicleAction() {
-		return $this->processFlow(new CreateVehicle(), $this->get('craueFormFlowDemoBundle.form.flow.createVehicle'));
+		return $this->processFlow(new CreateVehicle(), $this->get('craueFormFlowDemoBundle.form.flow.createVehicle'),
+				'@CraueFormFlowDemo/FormFlowDemo/createVehicle.html.twig');
 	}
 
 	/**
 	 * @Route("/create-topic/", name="_FormFlow_createTopic")
-	 * @Template
 	 */
 	public function createTopicAction() {
-		return $this->processFlow(new Topic(), $this->get('craueFormFlowDemoBundle.form.flow.createTopic'));
+		return $this->processFlow(new Topic(), $this->get('craueFormFlowDemoBundle.form.flow.createTopic'),
+				'@CraueFormFlowDemo/FormFlowDemo/createTopic.html.twig');
 	}
 
 	/**
 	 * @Route("/create-topic-dsn/", name="_FormFlow_createTopic_dynamicStepNavigation")
-	 * @Template
 	 */
 	public function createTopicWithDynamicStepNavigationAction() {
 		$flow = $this->get('craueFormFlowDemoBundle.form.flow.createTopic');
 		$flow->setAllowDynamicStepNavigation(true);
 
-		return $this->processFlow(new Topic(), $flow);
+		return $this->processFlow(new Topic(), $flow,
+				'@CraueFormFlowDemo/FormFlowDemo/createTopicWithDynamicStepNavigation.html.twig');
 	}
 
 	/**
 	 * @Route("/create-topic-redirect-after-submit/", name="_FormFlow_createTopic_redirectAfterSubmit")
-	 * @Template
 	 */
 	public function createTopicWithRedirectAfterSubmitAction() {
 		$flow = $this->get('craueFormFlowDemoBundle.form.flow.createTopic');
 		$flow->setAllowRedirectAfterSubmit(true);
 
-		return $this->processFlow(new Topic(), $flow);
+		return $this->processFlow(new Topic(), $flow,
+				'@CraueFormFlowDemo/FormFlowDemo/createTopicWithRedirectAfterSubmit.html.twig');
 	}
 
 	/**
 	 * @Route("/photo-upload/", name="_FormFlow_photoUpload")
-	 * @Template
 	 */
 	public function photoUploadAction() {
-		return $this->processFlow(new PhotoUpload(), $this->get('craueFormFlowDemoBundle.form.flow.photoUpload'));
+		return $this->processFlow(new PhotoUpload(), $this->get('craueFormFlowDemoBundle.form.flow.photoUpload'),
+				'@CraueFormFlowDemo/FormFlowDemo/photoUpload.html.twig');
 	}
 
-	protected function processFlow($formData, FormFlowInterface $flow) {
+	protected function processFlow($formData, FormFlowInterface $flow, $template) {
 		$flow->bind($formData);
 
 		$form = $submittedForm = $flow->createForm();
@@ -110,11 +108,11 @@ class FormFlowDemoController extends Controller {
 			return $this->redirect($this->generateUrl($request->attributes->get('_route'), $params));
 		}
 
-		return array(
+		return $this->render($template, array(
 			'form' => $form->createView(),
 			'flow' => $flow,
 			'formData' => $formData,
-		);
+		));
 	}
 
 }
